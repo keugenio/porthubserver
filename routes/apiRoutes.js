2250d8df85d99ebd871d96a8ef8ff017dca11f35
Kui <kui_eugenio@hotmail.com>
2018-01-03 21:28:33 -0700

    updated template route

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -3,7 +3,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/templates/:login', (req, res) => {
-  db.Template.find({login:req.params.login})
+  db.User.find({login:req.params.login})
   .populate('template')
   .sort({_id: -1})
   .then((data) => {
