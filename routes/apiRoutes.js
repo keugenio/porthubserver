359a7663832f9169d7c041cfe824332e3e846f09
Kui <kui_eugenio@hotmail.com>
2018-01-07 02:27:36 -0700

    added get route to just server a pdf for testing

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,3 +1,5 @@
+// import { escape } from "querystring";
+
 var db = require("../models");
 const express = require('express');
 const router = express.Router();
@@ -18,5 +20,31 @@ router.get('/user/:login', (req, res) =>{
     res.json("success");
   })
 })
+
+// convert pdf from html passed
+router.post('/retrieveResume', ((req, res) => {
+  const fs = require('fs');     
+  const pdf = require('html-pdf');
+  //var html = req.body.html;
+  
+  //const options = { "format": "Letter","border": {"top": "0","bottom":"0","left": "0","right":"0"},}; 
+  
+  // pdf.create("<b>bob</b>", options).toStream(function(err, stream){
+  //   stream.pipe(res);
+  // });
+
+  res.sendFile(__dirname + "/resume.pdf", (err) =>{
+    err ? console.log(err) : console.log("file sent");
+  });
+}));
+
+// test to just send pdf to client
+router.get('/retrieveResume', ((req, res) => {
+  const fs = require('fs');     
+
+  res.sendFile(__dirname + "/resume.pdf", (err) =>{
+    err ? console.log(err) : console.log("file sent");
+  });
+}));
 
 module.exports = router;
